d1a5f67d047e94645028b21eabba38f08c2b568b
aiqbal3 <aiqbal3@wisc.edu>
2021-10-04 03:10:18 -0500

    functional

---
+++ app/src/main/java/com/example/lab3_milestone2/MessageFragment.java
@@ -0,0 +1,29 @@
+package com.example.lab3_milestone2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MessageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MessageFragment extends Fragment {
+
+    public MessageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_message, container, false);
+    }
+}
